5bdaaf69d0946fd4058f647826760316dc2b6727
masakizk <robomasaki2@gmail.com>
2023-04-02 13:29:52 +0900

    feat: gin server

--- server.go
+++ server.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("HELLO WORLD")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "HELLO WORLD",
+		})
+	})
+	r.Run()
 }
